845efb8f4eab0578f2cc8c90c583b1b0e9cf2dc2
Nicolas Mussi <nmussidasilva@gmail.com>
2024-10-23 09:21:00 -0300

    add css ao codigo

--- views/login.php
+++ views/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" type='text/css' media='screen' href="main.css">
+    <link rel="stylesheet" type='text/css' media='screen' href="css/main.css">
 
 </head>
 <body>
