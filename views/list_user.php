41c84a989e72c2d53f6baec65658d56791d2d8e4
Nicolas Mussi <nmussidasilva@gmail.com>
2024-11-18 08:09:11 -0300

    add css

--- views/list_user.php
+++ views/list_user.php
@@ -27,29 +27,28 @@
                     <th>Ações</th>
                 </tr>
             </thead>
+            </thead>
             <tbody>
-                <?php foreach ($users as $user) ?>
-                <tr>
-                    <td><?= $user['id'] ?></td>
-                    <td><?= $user['nome'] ?></td>
-                    <td><?= $user['email'] ?></td>
-                    <td><?= $user['perfil'] ?></td>
-                    <td><?php if($_SESSION['perfil']  == 'admin' || $_SESSION['perfil']  == 'gestor' ):?>
-                        <a href="">Editar</a>
-                        <?php endif; ?>
-                        <?php if($_SESSION['perfil']  == 'admin'): ?>
-                            <a href="index.php?action=delete&id=<?=$user['id']?>" class="btn btn-delete" onclick="return confirm('Tem certeza que deseja excluir usuário?')">Excluir</a>
-                        <?php endif; ?>
- 
- 
-                    </td>
-                </tr>
- 
-               
-            </tbody>
+             <?php foreach($users as $user): ?> <!-- Corrigido o loop para exibir todos os usuários -->
+        <tr>
+        <td><?= $user['id'] ?></td>
+        <td><?= $user['nome'] ?></td>
+        <td><?= $user['email'] ?></td>
+        <td><?= $user['perfil'] ?></td>
+        <td>
+            <?php if($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor'): ?>
+                <a href="edit_users.php?id=<?= $user['id'] ?>" class="edit">Editar</a> <!-- Atualizado -->
+            <?php endif; ?>
+            <?php if($_SESSION['perfil'] == 'admin'): ?>
+                <a href="index.php?action=delete&id=<?= $user['id'] ?>" class="btn btn-delete" onclick="return confirm('Tem certeza que deseja excluir usuário?')">Excluir</a>
+            <?php endif; ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
         </table>
  
-        <a href="" class="btn">Voltar ao Dashboard</a>
+        <a href="index.php?action=VoltaraoDashboard" class="btn">Voltar ao Dashboard</a>
     </div>
 </body>
  
